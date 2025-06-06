fe6f86068af2573b1eee05196a85edfd50ca4fb1
tomaton-not <engineer.tomatonton@gmail.com>
2025-06-06 12:38:11 +0900

    修正対応_データベースのリセットと再マイグレート

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 bundle install
 bundle exec rails assets:precompile
-# bundle exec rake assets:clean
+
+# 本番DBを初期化する（テストデータのみの状態を前提）
+bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rails db:create
 bundle exec rails db:migrate
