d205fd1a480d425f0d5ac7c209748bbe44eaea14
tomaton-not <engineer.tomatonton@gmail.com>
2025-06-06 12:52:35 +0900

    修正対応_解消しなかったため当初の状況にいったん戻す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-
-# 本番DBを初期化する（テストデータのみの状態を前提）
-bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rails db:create
-bundle exec rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
