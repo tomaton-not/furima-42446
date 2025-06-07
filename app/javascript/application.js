14844e5178f69250833d94f7f21960174981394f
tomaton-not <engineer.tomatonton@gmail.com>
2025-06-07 16:29:30 +0900

    修正対応_JavaScript記述

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,6 @@ import "@hotwired/turbo-rails"
 import Rails from "@rails/ujs"
 Rails.start()
 
-import "./controllers"
-import "./item_price";
-import "./card"
+import "controllers"
+import "item_price"
+import "card"
