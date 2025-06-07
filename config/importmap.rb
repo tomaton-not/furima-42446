248cef3e10761448cdfa8ac4311c8594a920fec4
tomaton-not <engineer.tomatonton@gmail.com>
2025-06-07 15:36:23 +0900

    修正対応_JavaScript

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
-pin "item_price"
+pin "item_price", to: "item_price.js"
 pin "card", to: "card.js"
